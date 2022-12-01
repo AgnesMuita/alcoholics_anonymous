9dbbe970d7ace9ac6d548750c3daf3c9bb005491
AgnesMuita <wamwithamuita@gmail.com>
2022-12-01 10:19:59 +0300

    test db

--- app/db/db.py
+++ app/db/db.py
@@ -1,27 +1,19 @@
 from sqlalchemy.ext.declarative import declarative_base
-from typing import AsyncGenerator
-from sqlalchemy import (Column, Integer, MetaData, String, Table,
-                        create_engine, ARRAY, Boolean)
-from core.settings import settings
-from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy
 import databases
 
-from sqlmodel.ext.asyncio.session import AsyncSession
 
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Wamwitha2020@localhost/postgres'
+engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
-
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Wamwitha2020@localhost:5432/postgres'
-
-engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-# Base.metadata.create_all(engine)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 metadata = sqlalchemy.MetaData()
 metadata.create_all(engine)
-# Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(engine)
 
 def get_db():
     db=SessionLocal()
